73a21d3b6baffbb3c8d3ce7911620e665909ad03
Guillaume Mathias <guillaume.mathias2@safrangroup.com>
2018-12-13 10:03:15 +0100

    gestion configuration prod et dev dans webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,37 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 // npm i webpack-dev-server babel-loader @babel/core @babel/preset-env -D
 
-module.exports = {
-  entry: "./src/index.js",
-  output: {
-    filename: "app.js"
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./src/index.html"
-    })
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.m?js$/,
-        exclude: /(node_modules|bower_components)/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ["@babel/preset-env"]
-          }
-        }
-      },
-      {
-        test: /\.scss$/,
-        use: ["style-loader", "css-loader", "sass-loader"]
-      }
-    ]
-  }
-};
+module.exports = function(_, { mode }) {
+    return {
+        devtool: mode === 'development' ? 'source-map' : false,
+        entry: './src/index.js',
+        output: {
+            filename:
+                mode === 'production' ? 'app.[chunkHash].min.js' : 'app.js',
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: './src/index.html',
+            }),
+        ],
+        module: {
+            rules: [
+                {
+                    test: /\.m?js$/,
+                    exclude: /(node_modules|bower_components)/,
+                    use: {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['@babel/preset-env'],
+                        },
+                    },
+                },
+                {
+                    test: /\.scss$/,
+                    use: ['style-loader', 'css-loader', 'sass-loader'],
+                },
+            ],
+        },
+    }
+}
