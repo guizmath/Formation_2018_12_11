24d12fc4be775e1be82b38fd6faa97abf73e2b8b
Guillaume Mathias <guillaume.mathias2@safrangroup.com>
2018-12-13 10:04:23 +0100

    correction import css dans index.js

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
-import $ from "jquery";
-import "bootstrap";
-import { Clock } from "./clock";
+import $ from 'jquery'
+import 'bootstrap'
+import { Clock } from './clock'
 
 // C'est cet import qui lance l'ajout de la balise <style type="text/css">
-import css from "./style.scss";
+import './style.scss'
 
-var $mainBtn = $("#main-btn");
+var $mainBtn = $('#main-btn')
 
 $mainBtn.tooltip({
-  title: "Click me !",
-  placement: "right"
-});
+    title: 'Click me !',
+    placement: 'right',
+})
 
-const clockElt = document.querySelector("#clock");
+const clockElt = document.querySelector('#clock')
 const clock = new Clock({
-  container: clockElt
-});
+    container: clockElt,
+})
 
-clock.start();
+clock.start()
